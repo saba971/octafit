dabcf70c0e711c29c9ecb87b029a579fc0579cc6
saba971 <sabanayagam97@gmail.com>
2022-06-05 10:12:44 +0530

    commit

--- octafit/urls.py
+++ octafit/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from welcome import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,5 +39,8 @@ urlpatterns = [
     path('login',views.login,name="login"),
     path('signup',views.signup,name="signup"),
     path('edit1',views.edit1,name="edit1"),
-    path('signout',views.signout,name="signout")
+    path('signout',views.signout,name="signout"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+ 
