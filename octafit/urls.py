bfd50f1d3deab7a085f95ca20955535927f9fc01
saba971 <sabanayagam97@gmail.com>
2022-06-05 11:53:45 +0530

    commit

--- octafit/urls.py
+++ octafit/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from welcome import views
+from django.conf import settings
 
 from django.views.static import serve
 
