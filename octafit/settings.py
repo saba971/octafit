253d0bb64cb2cd401c224beceb724d7c4161aba0
saba971 <sabanayagam97@gmail.com>
2022-06-05 10:20:45 +0530

    commit

--- octafit/settings.py
+++ octafit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9-qo&f59pyl4dl^a_r_34gywgw+&!-+=1o$kx6bhr2l=^7#@l3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = FALSE
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','octafit.herokuapp.com']
 
 
 # Application definition
